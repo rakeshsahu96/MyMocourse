ce504d0775e058c73453b06ce4559d3d328dde7c
Rakesh Sahu <rakeshsahukwd96@gmail.com>
2020-11-06 18:53:45 +0530

    first

--- app/src/main/java/com/awizomtech/mymocourse/MainActivity.java
+++ app/src/main/java/com/awizomtech/mymocourse/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+
         webView.loadUrl("https://www.mocourse.mu/");
     }
 }

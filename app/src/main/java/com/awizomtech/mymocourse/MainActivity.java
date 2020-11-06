b5ec33271fcf8e0852d80cb5827b47916fee1d26
Rakesh Sahu <rakeshsahukwd96@gmail.com>
2020-11-06 18:30:07 +0530

    Initial commit

---
+++ app/src/main/java/com/awizomtech/mymocourse/MainActivity.java
@@ -0,0 +1,22 @@
+package com.awizomtech.mymocourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://mocourse.mu/");
+    }
+}
